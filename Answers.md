67183d795f46681108821a259067c75c058b4aad
Alex Cassell <alex@simulism.net>
2017-12-15 15:52:06 -0500

    Not yet complete

--- Answers.md
+++ Answers.md
@@ -1,1 +1,4 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
+Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
+What does bcrypt do in order to prevent attacks?
+What are the three parts of the JSON Web Token?
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,10 +2,10 @@ const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
-  // there should be a user object set on req
-  // use that req.user object to create a user and save it to our Mongo instance.
+  const { username } = req.body;
+  const passwordHash = req.password;
+  const newUser = new User({ username, passwordHash });
+
 };
 
-module.exports = {
-  createUser
-};
+export default createUser;
