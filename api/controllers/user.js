7db23b5bf5a0303060bef1b1054df3366a3da61c
Alex Cassell <alex@simulism.net>
2017-12-15 17:40:49 -0500

    what i have now

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,7 +5,14 @@ const createUser = (req, res) => {
   const { username } = req.body;
   const passwordHash = req.password;
   const newUser = new User({ username, passwordHash });
-
+  newUser.save((err, savedUser) => {
+    if (err) {
+      res.status(422);
+      res.json({ 'Missing Required Fields.': err.message });
+      return;
+    }
+    res.json(savedUser);
+  });
 };
 
 export default createUser;
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -5,6 +5,15 @@ const User = require('../models/userModels');
 const { mysecret } = require('../../config');
 const SaltRounds = 11;
 
+const sendError = (err, res) => {
+  res.status(422);
+  if (err && err.message) {
+    res.json({ message: err.message, stack: err.stack });
+  } else {
+    res.json({ error: err });
+  }
+};
+
 const authenticate = (req, res, next) => {
   const token = req.get('Authorization');
   if (token) {
@@ -22,20 +31,42 @@ const authenticate = (req, res, next) => {
 
 const encryptUserPW = (req, res, next) => {
   const { username, password } = req.body;
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // Once the password is encrypted using bcrypt, you'll need to save the user the DB.
-  // Once the user is set, take the savedUser and set the returned document from Mongo on req.user
-  // call next to head back into the route handler for encryptUserPW
+  if (!password) {
+    sendError('Incorrect entry, Please try again.', res);
+    return;
+  }
+  bcrypt
+    .hash(password, SaltRounds)
+    .then((pw) => {
+      req.password = pw;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
 
 const compareUserPW = (req, res, next) => {
   const { username, password } = req.body;
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // TODO: Fill this middleware in with the Proper password comparing, bcrypt.compare()
-  // You'll need to find the user in your DB
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-  // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+  if (!username) {
+    sendError('Incorrect entry, Please try again.', res);
+    return;
+  }
+  User.findOne({ username }, (err, user) => {
+    if (err || user === null) {
+      sendError('Incorrect entry, Please try again.', res);
+      return;
+    }
+    const hashed = user.passwordHash;
+    bcrypt
+      .compare(password, hashed)
+      .then((response) => {
+        if (!response) throw new Error();
+        req.username = user.username;
+        next();
+      })
+  });
+  
 };
 
 module.exports = {
